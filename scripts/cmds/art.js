6a2c89d22c049acf9ce10aeba8207b44db70a8fa
Tarek-bot-009 <tareksarkar620@gmail.com>
2024-10-25 11:10:19 +0600

    Update art.js

--- scripts/cmds/art.js
+++ scripts/cmds/art.js
@@ -1,46 +1,42 @@
-const axios = require('axios');
-const fs = require('fs');
+const axios = require("axios");
 
 module.exports = {
   config: {
     name: "art",
-    version: "1.0",
-    author: "Who's Deku", //nihan
-    countDown: 5,
     role: 0,
-    shortDescription: {
-      en: ""
-    },
-    longDescription: {
-      en: "al"
-    },
-    category: "system",
+    author: "OtinXSandip",
+    countDown: 0,
+    longDescription: "Art images",
+    category: "AI",
+    guide: {
+      en: "${pn} reply to an image with a prompt and choose model 1 - 10"
+    }
   },
-  
-  onStart: async function ({ api, event, args }) {
-    const { threadID, messageID } = event;
-    if (event.type == "message_reply") {
-      var t = event.messageReply.attachments[0].url;
-    } else {
-      var t = args.join(" ");
+  onStart: async function ({ message, api, args, event }) {
+    const text = args.join(' ');
+    
+    if (!event.messageReply || !event.messageReply.attachments || !event.messageReply.attachments[0]) {
+      return message.reply("reply to image");
     }
-    try {
-      api.sendMessage("Generating...", threadID, messageID);
-      const r = await axios.get("https://free-api.ainz-sama101.repl.co/canvas/toanime?", {
-        params: {
-          url: encodeURI(t)
-        }
+
+    const imgurl = encodeURIComponent(event.messageReply.attachments[0].url);
+
+    const [model] = text.split('|').map((text) => text.trim());
+    const puti = model || "6";
+        
+    api.setMessageReaction("⏰", event.messageID, () => {}, true);
+    const lado = `https://sandipbaruwal.onrender.com/art?url=${imgurl}&model=${puti}`;
+
+   const baby = await require('tinyurl').shorten(lado);
+
+message.reply("✅| Generating please wait.", async (err, info) => {
+      const attachment = await global.utils.getStreamFromURL(lado);
+      message.reply({  body: `${baby}`,
+        attachment: attachment
       });
-      const result = r.data.result.image_data;
-      let ly = __dirname + "/cache/anime.png";
-      let ly1 = (await axios.get(result, {
-        responseType: "arraybuffer"
-      })).data;
-      fs.writeFileSync(ly, Buffer.from(ly1, "utf-8"));
-      return api.sendMessage({ attachment: fs.createReadStream(ly) }, threadID, () => fs.unlinkSync(ly), messageID);
-    } catch (e) {
-      console.log(e.message);
-      return api.sendMessage("Something went wrong: " + e.message, threadID, messageID);
-    }
+      let ui = info.messageID;          
+      message.unsend(ui);
+      api.setMessageReaction("✅", event.messageID, () => {}, true);
+    });
   }
 };
